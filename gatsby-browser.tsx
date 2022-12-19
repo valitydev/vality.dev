d991be7d62150d49ccd5faf3565b34010aae411a
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2022-12-19 21:38:51 +0600

    init mui container

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -3,6 +3,21 @@ import type { GatsbyBrowser } from "gatsby";
 import { Layout } from "./src/components/Layout";
 import { GLOBAL_STYLE } from "./src/styles/global-styles";
 import { Global } from "@emotion/react";
+import { createTheme, ThemeProvider } from "@mui/system";
+
+const OFFSET = 24 * 2;
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xl: 944 + OFFSET,
+      lg: 944 + OFFSET,
+      md: 686 + OFFSET,
+      sm: 560 + OFFSET,
+      xs: 288 + OFFSET,
+    },
+  },
+});
 
 export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({
   element,
@@ -10,7 +25,9 @@ export const wrapPageElement: GatsbyBrowser["wrapPageElement"] = ({
   return (
     <>
       <Global styles={GLOBAL_STYLE} />
-      <Layout>{element}</Layout>
+      <ThemeProvider theme={theme}>
+        <Layout>{element}</Layout>
+      </ThemeProvider>
     </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,13 @@ import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import UnstyledLogo from "../assets/logo.svg";
 import styled from "@emotion/styled";
-
-const Main = styled.div``;
+import { Container, Box } from "@mui/system";
 
 const Logo = styled(UnstyledLogo)`
   height: 32px;
 `;
 
-const Header = styled.header`
-  background-color: #000c1f;
-`;
+const Header = styled.header``;
 
 const Menu = styled.ul`
   color: #fff;
@@ -19,17 +16,19 @@ const Menu = styled.ul`
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <Main>
-      <Header>
-        <Logo />
-        <Menu>
-          <li>a</li>
-          <li>a</li>
-          <li>a</li>
-          <li>a</li>
-        </Menu>
-      </Header>
-    </Main>
+    <Box sx={{ backgroundColor: "#000c1f" }}>
+      <Container fixed>
+        <Header>
+          <Logo />
+          <Menu>
+            <li>a</li>
+            <li>a</li>
+            <li>a</li>
+            <li>a</li>
+          </Menu>
+        </Header>
+      </Container>
+    </Box>
   );
 };
 
