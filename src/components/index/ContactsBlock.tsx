0bf8851c0a667f56e51418360449d8f4cd09b2dd
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2022-12-27 11:37:56 +0600

    hide data

--- src/components/index/ContactsBlock.tsx
+++ src/components/index/ContactsBlock.tsx
@@ -15,85 +15,97 @@ import contactsBgSrc from "../../images/contacts-bg.svg";
 import { BackgroundImageBox } from "../BackgroundImageBox";
 import { IconButton } from "../IconButton";
 import config from "../../../config.json";
+import { useState, useEffect } from "react";
 
 export const ContactsBlock: React.FC<React.ComponentProps<typeof Block>> = (
   props
-) => (
-  <Block {...props} inverted sx={{ paddingBottom: "0 !important" }}>
-    <BackgroundImageBox src={contactsBgSrc} right={-1100} bottom={-700}>
-      <Stack spacing={25}>
-        <Stack direction="row">
-          <Stack flex={1} spacing={5.5}>
-            <Box sx={{ typography: "h3", color: "#fff" }}>
-              <Trans>Contacts</Trans>
-            </Box>
-            <Stack spacing={4.5} direction="row">
-              {config.contacts.email && (
-                <Stack spacing={2}>
-                  <Box>
-                    <Trans>Email</Trans>
-                  </Box>
-                  <Button
-                    endIcon={<Email />}
-                    href={`mailto:${config.contacts.email}`}
-                  >
-                    {config.contacts.email}
-                  </Button>
-                </Stack>
-              )}
-              {config.contacts.telegram && (
-                <Stack spacing={2}>
-                  <Box>
-                    <Trans>Telegram</Trans>
-                  </Box>
-                  <Button
-                    endIcon={<Telegram />}
-                    href={config.contacts.telegram}
-                  >
-                    Vality
-                  </Button>
-                </Stack>
-              )}
-            </Stack>
-            <Stack spacing={2}>
-              <Box>
-                <Trans>Social media</Trans>
+) => {
+  const [realtimeConfig, setRealtimeConfig] = useState({
+    contacts: {},
+  } as typeof config);
+
+  useEffect(() => {
+    /* Load contacts only in real time to hide from scrappers (which don't know how to use JS) */
+    setRealtimeConfig(config);
+  }, []);
+
+  return (
+    <Block {...props} inverted sx={{ paddingBottom: "0 !important" }}>
+      <BackgroundImageBox src={contactsBgSrc} right={-1100} bottom={-700}>
+        <Stack spacing={25}>
+          <Stack direction="row">
+            <Stack flex={1} spacing={5.5}>
+              <Box sx={{ typography: "h3", color: "#fff" }}>
+                <Trans>Contacts</Trans>
               </Box>
-              <Stack spacing={4} direction="row">
-                <IconButton inverted href={config.contacts.linkedin}>
-                  <Linkedin />
-                </IconButton>
-                <IconButton inverted href={config.contacts.github}>
-                  <Github />
-                </IconButton>
+              <Stack spacing={4.5} direction="row">
+                {realtimeConfig.contacts.email && (
+                  <Stack spacing={2}>
+                    <Box>
+                      <Trans>Email</Trans>
+                    </Box>
+                    <Button
+                      endIcon={<Email />}
+                      href={`mailto:${realtimeConfig.contacts.email}`}
+                    >
+                      {realtimeConfig.contacts.email}
+                    </Button>
+                  </Stack>
+                )}
+                {realtimeConfig.contacts.telegram && (
+                  <Stack spacing={2}>
+                    <Box>
+                      <Trans>Telegram</Trans>
+                    </Box>
+                    <Button
+                      endIcon={<Telegram />}
+                      href={realtimeConfig.contacts.telegram}
+                    >
+                      Vality
+                    </Button>
+                  </Stack>
+                )}
+              </Stack>
+              <Stack spacing={2}>
+                <Box>
+                  <Trans>Social media</Trans>
+                </Box>
+                <Stack spacing={4} direction="row">
+                  <IconButton inverted href={realtimeConfig.contacts.linkedin}>
+                    <Linkedin />
+                  </IconButton>
+                  <IconButton inverted href={realtimeConfig.contacts.github}>
+                    <Github />
+                  </IconButton>
+                </Stack>
               </Stack>
             </Stack>
-          </Stack>
-          <Box flex={1}>
-            <ContactsImg />
-          </Box>
-        </Stack>
-        <Stack spacing={5.5} sx={{ paddingBottom: 4.5 }}>
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Logo style={{ width: "auto" }} height={32} />
-            <LanguageSelector />
-          </Stack>
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <Box>
-              &copy; <Trans>All rights reserved</Trans>
+            <Box flex={1}>
+              <ContactsImg />
             </Box>
-            <AppNav />
+          </Stack>
+          <Stack spacing={5.5} sx={{ paddingBottom: 4.5 }}>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Logo style={{ width: "auto" }} height={32} />
+              <LanguageSelector />
+            </Stack>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Box>
+                &copy; <Trans>All rights reserved</Trans>
+              </Box>
+              <AppNav />
+            </Stack>
           </Stack>
         </Stack>
-      </Stack>
-    </BackgroundImageBox>
-  </Block>
-);
+      </BackgroundImageBox>
+    </Block>
+  );
+};
