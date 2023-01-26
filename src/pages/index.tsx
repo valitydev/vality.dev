d4b3fbacd723bbadd4fb5543703fceb874d6e8f2
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2023-01-27 00:47:33 +0600

    upd index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,24 @@
 import * as React from "react";
 
-import { Box } from "@mui/system";
 import type { HeadFC, PageProps } from "gatsby";
 import { graphql } from "gatsby";
 
 import { PAGE_IDS } from "~/components/AppNav";
+import { AntifraudBlock } from "~/components/index/AntifraudBlock/AntifraudBlock";
+import { ContactsBlock } from "~/components/index/ContactsBlock";
 import { HeaderBlock } from "~/components/index/HeaderBlock";
-
-const AntifraudBlock = React.lazy(
-  () => import("~/components/index/AntifraudBlock/AntifraudBlock")
-);
-const ContactsBlock = React.lazy(
-  () => import("~/components/index/ContactsBlock")
-);
-const ProcessingBlock = React.lazy(
-  () => import("~/components/index/ProcessingBlock")
-);
-const OurProductsBlock = React.lazy(
-  () => import("~/components/index/OurProductsBlock")
-);
+import { OurProductsBlock } from "~/components/index/OurProductsBlock";
+import { ProcessingBlock } from "~/components/index/ProcessingBlock";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <Box>
+    <>
       <HeaderBlock />
       <OurProductsBlock id={PAGE_IDS.ourProducts} />
       <ProcessingBlock id={PAGE_IDS.processing} />
       <AntifraudBlock id={PAGE_IDS.antifraud} />
       <ContactsBlock id={PAGE_IDS.contacts} />
-    </Box>
+    </>
   );
 };
 
